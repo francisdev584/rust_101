c149b8600b7e4c19bd22b90cf6f63eb99bf6313e
Francinildo Rodrigues <francis584@gmail.com>
2025-02-02 12:56:52 -0300

    learning basics: if, else if and else flow

---
+++ src/bin/a3b_if_else_if_else_flow.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let number = 4;
+
+    if number > 5 {
+        println!("Greater than 5")
+    } else if number < 5 {
+        println!("Less than 5")
+    }else {
+        println!("Equal to 5")
+    }
+
+}
