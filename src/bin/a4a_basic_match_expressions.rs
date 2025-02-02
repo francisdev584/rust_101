fc09edca4e08ed8260bc3e91de77804b223b6376
Francinildo Rodrigues <francis584@gmail.com>
2025-02-02 13:25:51 -0300

    learning basics: match expressions

---
+++ src/bin/a4a_basic_match_expressions.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let my_boolean = true;
+
+    match my_boolean {
+        true => println!("it's true!"),
+        false => println!("it's false"),
+    }
+
+}
---
+++ src/bin/a4b_decision_making_with_match.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let variable = 22;
+
+    match variable {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("other")
+    }
+}
