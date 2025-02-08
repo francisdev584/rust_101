5418a6baedf1efaa4d4b102f18dbd840879bd33e
Francinildo Rodrigues <francis584@gmail.com>
2025-02-07 23:03:05 -0300

    learning basics: rust documentation

---
+++ src/bin/a17_documentation.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // Run "rustup doc" to see API Documentation
+    let my_str = "essa é MINHA string";
+
+    println!("uppercase: {:?}", my_str.to_uppercase());
+    println!("lowercase: {:?}", my_str.to_lowercase());
+
+}
